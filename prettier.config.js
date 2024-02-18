258c04ccaccc4ce9bfbe9e48975f5cba6ece8096
Wolfe Weeks <wolfesevenweeks@gmail.com>
2024-02-18 17:35:20 -0600

    Add global CSS and fonts imports, update body class, and add hero images

--- prettier.config.js
+++ prettier.config.js
@@ -3,4 +3,6 @@ const styleguide = require('@vercel/style-guide/prettier');
 module.exports = {
   ...styleguide,
   plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
+  printWidth: 80,
+  trailingComma: 'all',
 };
